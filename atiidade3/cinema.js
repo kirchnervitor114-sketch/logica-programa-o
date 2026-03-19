063fa4d4c7bcc30604b886734f7853812cee9b22
ian <inatmusel55viper@gmail.com>
2026-03-19 12:23:08 -0300

    arrays biodimensionais

---
+++ atiidade3/cinema.js
@@ -0,0 +1,9 @@
+ let assentos = [
+ ["L", "L","O"],
+ ["L", "O", "L"],
+  ]; 
+
+  let cadeiraMeio = assentos [1][1];
+   console.log ("A cadeira do meio está " + cadeiraMeio); 
+   assentos[0][0] = "o"; 
+   console.log("O assento em [0][0] está " + assentos[0][0])
